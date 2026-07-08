6c2930c64ca7b8cdd90899f3c1c432140dc7e44f
Saksham Kulshrestha <25etim44@uohyd.ac.in>
2026-07-08 19:58:29 +0530

    Time: 15 ms (11.36%) | Memory: 21 MB (5.52%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& v) {
+        map<int, int> map;
+        for(int i=0; i<v.size(); i++){
+            if(map.count(v[i]) > 0){
+                map[v[i]]++;
+            }
+            else{
+                map.emplace(v[i], 1);
+            }
+        }
+
+        int idx = 0;
+        int k = 0;
+        for(auto val : map){
+            if(val.second > 1){
+                v[idx++] = val.first;
+                v[idx++] = val.first;
+
+                k += 2;
+            }
+            else{
+                v[idx++] = val.first; 
+                k++;
+            }
+        }
+
+        return k;
+    }
+};
