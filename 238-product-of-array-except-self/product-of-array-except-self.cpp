f63385b957c90086aa8153fd039292f14c7fdfe2
Saksham Kulshrestha <25etim44@uohyd.ac.in>
2026-07-09 20:18:58 +0530

    Time: 3 ms (37.87%) | Memory: 40.2 MB (61.08%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,60 +2,20 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& v) {
         int n = v.size();
-        bool isOneZero = false;
-        bool isTwoZero = false;
-        bool noZero = false;
-
-        int zeroCount = 0;
-        for(int i=0; i<n; i++){
-            if(v[i] == 0){
-                zeroCount++;
-            }
-            if(zeroCount > 1){
-                isTwoZero = true;
-                break;
-            }
-        }
-
-        if(zeroCount == 1){
-            isOneZero = true;
-        }
-        else if(zeroCount == 0){
-            noZero = true;
-        }
-
-        if(isTwoZero){
-            vector<int> ans(n, 0);
-            return ans;
-        }
-        else if(isOneZero){
-            int prod = 1;
-            int idx;
-            for(int i=0; i<n; i++){
-                if(v[i] == 0){
-                    idx = i;
-                }
-                else{
-                    prod *= v[i];
-                }
-            }
-
-            vector<int> ans(n, 0);
-            ans[idx] = prod;
-
-            return ans;
-        }
-        
         vector<int> ans(n);
-        int prod = 1;
+
+        int leftPro = 1;
         for(int i=0; i<n; i++){
-            prod *= v[i];
+            ans[i] = leftPro;
+            leftPro *= v[i];
         }
 
-        for(int i=0; i<n; i++){
-            ans[i] = prod/v[i];
+        int rightPro = 1;
+        for(int i=n-1; i>=0; i--){
+            ans[i] *= rightPro;
+            rightPro *= v[i];
         }
+
         return ans;
-
     }
 };
