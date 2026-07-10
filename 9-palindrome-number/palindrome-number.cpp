493455445170fd979e133d85b62021164e8dd9de
Saksham Kulshrestha <25etim44@uohyd.ac.in>
2026-07-10 21:16:37 +0530

    Time: 3 ms (44.87%) | Memory: 9.8 MB (19.94%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+
+        string s;
+        while(x>0){
+            s += x%10;
+            x /= 10;
+        }
+
+        int st=0, end=s.length()-1;
+        while(st<end){
+            if(s[st++] == s[end--]){
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
