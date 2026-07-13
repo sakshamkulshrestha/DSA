f645946e9ef2783a6ba8f53d0da40517500ba683
Saksham Kulshrestha <25etim44@uohyd.ac.in>
2026-07-13 08:39:33 +0530

    Time: 8 ms (15.33%) | Memory: 11.7 MB (75.00%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -7,22 +7,12 @@ public:
         int l2 = m.length();
 
         for(int i=0; i<l2; i++){
-            if(map.count(m[i])){
-                map[m[i]]++;
-            }
-            else{
-                map.emplace(m[i], 1);
-            }
+            map[m[i]]++;
         }
 
         for(int i=0; i<l1; i++){
-            if(map.count(r[i])){
-                if(map[r[i]] == 1){
-                    map.erase(r[i]);
-                }
-                else{
-                    map[r[i]]--;
-                }
+            if(map[r[i]]){
+                map[r[i]]--;
             }
             else{
                 return false;
