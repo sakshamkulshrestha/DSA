223d38bab92fb38d27fc26ba28a1c62581b564c7
Saksham Kulshrestha <25etim44@uohyd.ac.in>
2026-08-21 22:14:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (79.88%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    int addDigits(int n){
-        while (n > 9) {
-            int val = 0;
-            while(n > 0){
-                val += n % 10;
-                n /= 10;
-            }
-            n = val;
+    int addDigits(int n) {
+        if(n == 0){
+            return 0;
         }
-        
-        return n;
+        if(n % 9 == 0){
+            return 9;
+        }
+        return n % 9;
     }
 };
