473cb94adefbd93ad06776ee31c50bc7a5e4fe93
Saksham Kulshrestha <25etim44@uohyd.ac.in>
2026-08-31 20:38:25 +0530

    Time: 2 ms (19.11%) | Memory: 8.7 MB (54.39%) - LeetSync

--- 4321-digit-frequency-score/digit-frequency-score.cpp
+++ 4321-digit-frequency-score/digit-frequency-score.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
     int digitFrequencyScore(int n) {
-        unordered_map<int, int> map;
-
-        int temp = n;
-        while(temp > 0){
-            int val = temp%10;
-            
-            if(map.count(val)){
-                map[val]++;
-            }
-            else{
-                map[val] = 1;
-            }
-
-            temp /= 10;
+        int sum = 0;
+        while(n>0){
+            sum += n%10;
+            n /= 10;
         }
 
-        int ans = 0;
-        for(auto val : map){
-            int sum = val.first * val.second;
-            ans += sum;
-        }
-
-        return ans;
+        return sum;
     }
 };
