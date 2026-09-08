d58d2e6f0561c3b216cbba99bc7c468a3ee965a3
Saksham Kulshrestha <25etim44@uohyd.ac.in>
2026-09-08 08:16:56 +0530

    Time: 9 ms (9.49%) | Memory: 33.8 MB (7.65%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& v) {
-        vector<int> ans;
-        unordered_map<int, int> map;
         int n = v.size();
+        sort(v.begin(), v.end());
+        vector<int> ans;
 
-        if(n == 0){
-            return ans;
-        }
-
-        int max = v[0];
-        int min = v[0];
+        if(n == 0) return ans;
+        unordered_map<int, bool> map;
 
         for(int val : v){
-            if(val > max){
-                max = val;
-            }
-            else if(val < min){
-                min = val;
-            }
-
-            map[val] = 1;
+            map[val] = true;
         }
 
-        for(int i=min; i<=max; i++){
-            if(map.count(i)){
-                continue;
-            }
-            else{
-                ans.push_back(i);
-            }
+        for(int i=v[0]; i<=v[n-1]; i++){
+            if(!map.count(i)) ans.push_back(i);
         }
-        
+
         return ans;
     }
 };
