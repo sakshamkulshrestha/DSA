5ff1361726b4aa2d0ff2488d551a3dcbe22ed413
Saksham Kulshrestha <25etim44@uohyd.ac.in>
2026-09-19 19:40:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (35.91%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
-    int arrangeCoins(int n) {
-        int c = 0;
-        int i = 1;
-        while(n > 0){
-            c++;
-            n -= i;
-            i++;
-
-            if(n < i) break;
-        }
-
-        return c;
+    int arrangeCoins(long long n) {
+        return (sqrt(8*n + 1) - 1) / 2;
     }
 };
